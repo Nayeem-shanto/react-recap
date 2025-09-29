285113c5e698df7901ebe41f51d4b97204c24dc6
Nayeem Hasan Shanto <snayeemhasan@gmail.com>
2025-09-30 00:51:20 +0600

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,39 @@
 
 import "./App.css";
 
+
 function App() {
   
 
   return (
     <>
-      <h1>Welcome to the world of REACT JS</h1>
-      <div className="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+      <h1 className="font-bold text-4xl text-center w-10/12 mx-auto pt-10">Welcome to the world of REACT JS</h1>
+
+      <section className="bg-sky-300 w-10/12 mx-auto mt-10 px-5">
+          <div>
+            <p>How to use props in react js</p>
+
+          </div>
+          <div className="grid grid-cols-4  ">
+            <Developer name="Allen Blake" tech="Javascript"></Developer>
+            <Developer name="David Smith" tech="C#"></Developer>
+            <Developer name="Michel frik" tech="Java"></Developer>
+            <Developer name="Pitter parker" tech="PHP"></Developer>
+          </div>
+      </section>
+      
     </>
   );
+}
+
+function Developer(props){
+  console.log(props)
+  return(
+    <div className="border-2 border-amber-400 p-4  text-2xl">
+      <h3 >Name: <span className="font-semibold">{props.name}</span> </h3>
+      <h3>Technology: {props.tech} </h3>
+    </div>
+  )
 }
 
 export default App;
